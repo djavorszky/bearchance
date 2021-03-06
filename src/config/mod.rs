ac76fe438d537d649495f0cd0875ad264f592f3c
Daniel Javorszky <daniel.javorszky@gmail.com>
2021-03-06 18:04:57 +0100

    Read config from file

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,16 +1,18 @@
 use bevy::ecs::FromResources;
+use serde::Deserialize;
+use serde_json;
 
-pub type MaxSpeed = f32;
+use std::fs;
 
-#[derive(Debug)]
+#[derive(Debug, Deserialize)]
 pub struct GameConfig {
-    pub default_max_speed: MaxSpeed,
+    pub default_max_speed: f32,
 }
 
 impl FromResources for GameConfig {
     fn from_resources(_resources: &bevy::ecs::Resources) -> Self {
-        GameConfig {
-            default_max_speed: 5.,
-        }
+        let config = fs::read_to_string("config.json").expect("Failed reading config file");
+
+        serde_json::from_str(config.as_str()).expect("failed parsing configuration")
     }
 }
